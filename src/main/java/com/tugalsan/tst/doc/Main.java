645e34e966a23e389b990e57e1d042bf0036e3ba
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-11 11:02:16 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/tst/doc/Main.java
@@ -0,0 +1,13 @@
+package com.tugalsan.tst.doc;
+
+
+public class Main {
+
+    private static final TS_Log d = TS_Log.of(true, Main.class);
+
+    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.tst.doc
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.tst.doc-1.0-SNAPSHOT-jar-with-dependencies.jar
+    public static void main(String... s) {
+		
+    }
+}
