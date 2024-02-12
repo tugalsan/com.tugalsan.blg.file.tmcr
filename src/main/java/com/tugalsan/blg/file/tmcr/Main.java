11afe127d8260f3f3e585139410112fd8f386d1c
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-12 06:41:21 +0300

    .

---
+++ src/main/java/com/tugalsan/blg/file/tmcr/Main.java
@@ -0,0 +1,15 @@
+package com.tugalsan.blg.file.tmcr;
+
+import com.tugalsan.api.log.server.TS_Log;
+
+
+public class Main {
+
+    private static final TS_Log d = TS_Log.of(true, Main.class);
+
+    //cd C:\me\codes\com.tugalsan\tst\com.tugalsan.blg.doc
+    //java --enable-preview --add-modules jdk.incubator.vector -jar target/com.tugalsan.blg.doc-1.0-SNAPSHOT-jar-with-dependencies.jar
+    public static void main(String... s) {
+		
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
-module com.tugalsan.blg.doc {
+module com.tugalsan.blg.file.tmcr {
     requires com.tugalsan.api.log;
+    requires com.tugalsan.api.file.tmcr;
 }
