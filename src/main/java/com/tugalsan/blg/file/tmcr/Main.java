1b1c9d99577ea72a3f0e8fafa9849dbae2226144
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-02-20 17:00:44 +0300

    .

--- src/main/java/com/tugalsan/blg/file/tmcr/Main.java
+++ src/main/java/com/tugalsan/blg/file/tmcr/Main.java
@@ -6,8 +6,16 @@ import com.tugalsan.api.callable.client.TGS_CallableType5;
 import com.tugalsan.api.file.common.server.TS_FileCommonBall;
 import com.tugalsan.api.list.client.TGS_ListUtils;
 import com.tugalsan.api.log.server.TS_Log;
+import com.tugalsan.api.runnable.client.TGS_RunnableType1;
+import com.tugalsan.api.runnable.client.TGS_RunnableType2;
+import com.tugalsan.api.sql.conn.server.TS_SQLConnAnchor;
+import com.tugalsan.api.sql.conn.server.TS_SQLConnConfig;
 import com.tugalsan.api.url.client.TGS_Url;
 import com.tugalsan.lib.file.tmcr.client.TGS_FileTmcrTypes;
+import com.tugalsan.lib.file.tmcr.server.code.page.TS_FileTmcrCodePageWriter;
+import com.tugalsan.lib.file.tmcr.server.code.parser.TS_FileTmcrParser;
+import com.tugalsan.lib.file.tmcr.server.code.table.TS_FileTmcrCodeTableWriter;
+import com.tugalsan.lib.file.tmcr.server.code.text.TS_FileTmcrCodeTextWriter;
 import com.tugalsan.lib.file.tmcr.server.file.TS_FileTmcrFileHandler;
 import java.nio.file.Path;
 import java.util.List;
@@ -21,10 +29,14 @@ public class Main {
     public static void main(String... s) {
 
         List<String> macroLines = TGS_ListUtils.of(
-                "INSERT_PAGE A4 PORTRAIT",
-                "BEGIN_TEXT LEFT",
-                "ADD_TEXT Merhaba Gardaş",
-                "END_TEXT"
+                TS_FileTmcrCodePageWriter.INSERT_PAGE(4, true),
+                TS_FileTmcrCodeTableWriter.BEGIN_TABLE(1),
+                TS_FileTmcrCodeTableWriter.BEGIN_TABLECELL(1, 1, null),
+                TS_FileTmcrCodeTextWriter.BEGIN_TEXT_LEFT(),
+                TS_FileTmcrCodeTextWriter.ADD_TEXT("Tuğalsan Karabacak ♠☀☁☃☎☛ ŞşİiIıÜüÖöÇçŞşĞğ"),
+                TS_FileTmcrCodeTextWriter.END_TEXT(),
+                TS_FileTmcrCodeTableWriter.END_TABLECELL(),
+                TS_FileTmcrCodeTableWriter.END_TABLE()
         );
         String username = "myUserName";
         String tablename = "myTableName";
@@ -48,19 +60,19 @@ public class Main {
         Path fontPathItalic = Path.of("D:\\xampp_data\\DAT\\PUB\\FONT\\Roboto-Italic.ttf");
         Path fontPathRegular = Path.of("D:\\xampp_data\\DAT\\PUB\\FONT\\Roboto-Regular.ttf");
         TGS_Url customDomain = TGS_Url.of("https://localhost:8443");
-        TGS_Url favIconPng = TGS_Url.of("https://localhost:8443/res-other/res/mesametal_com/favicon/mesametal-dark-16x16.png");
-        String domainName = "localhost";
-        TGS_CallableType1<TGS_Url, TGS_Url> manipulateInjectCode = _url -> _url;
+        TGS_Url favIconPng = TGS_Url.of("https://localhost:8443/favicon/dark-16x16.png");
+        String domainName = "localhost";//WHY NOT PARSE FROM customDomain(?)
         Path dirDatTbl = Path.of("D:\\xampp_data\\DAT\\TBL");
         Path dirDatPub = Path.of("D:\\xampp_data\\DAT\\PUB");
         Path dirDatUsr = Path.of("D:\\xampp_data\\DAT\\USR\\admin");
         Path dirDatUsrTmp = Path.of("D:\\xampp_data\\DAT\\USR\\admin\\tmp");
-        TGS_CallableType5<List<String>, String, String, Long, String, Boolean> libTableFileList_getFileNames_DataIn = (a, b, c, d, e) -> TGS_ListUtils.of();
-        TGS_CallableType2<Path, String, String> libTableFileDir_datTblTblnameColname = (a, b) -> dirDatUsrTmp;
-        TGS_CallableType2<TGS_Url, String, Boolean> libTableFileGetUtils_urlUsrTmp = (a, b) -> favIconPng;
-        TGS_CallableType1<String, CharSequence> libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE = a -> customDomain.toString();
-        TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_PUBLIC = a -> customDomain.toString();
-        TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER = a -> customDomain.toString();
+        TGS_CallableType1<TGS_Url, TGS_Url> manipulateInjectCode = _url -> _url;//SKIP LEGACY CODE
+        TGS_CallableType5<List<String>, String, String, Long, String, Boolean> libTableFileList_getFileNames_DataIn = (a, b, c, d, e) -> TGS_ListUtils.of();//SKIP LEGACY CODE
+        TGS_CallableType2<Path, String, String> libTableFileDir_datTblTblnameColname = (a, b) -> dirDatUsrTmp;//SKIP LEGACY CODE
+        TGS_CallableType2<TGS_Url, String, Boolean> libTableFileGetUtils_urlUsrTmp = (a, b) -> favIconPng;//SKIP LEGACY CODE
+        TGS_CallableType1<String, CharSequence> libTableServletUtils_URL_SERVLET_FETCH_TBL_FILE = a -> customDomain.toString();//SKIP LEGACY CODE
+        TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_PUBLIC = a -> customDomain.toString();//SKIP LEGACY CODE
+        TGS_CallableType1<String, CharSequence> libFileServletUtils_URL_SERVLET_FETCH_USER = a -> customDomain.toString();//SKIP LEGACY CODE
         var fileCommonBall = new TS_FileCommonBall(
                 macroLines, username,
                 tablename, selectedId,
@@ -77,12 +89,21 @@ public class Main {
                 libFileServletUtils_URL_SERVLET_FETCH_PUBLIC,
                 libFileServletUtils_URL_SERVLET_FETCH_USER
         );
+        var anchor = new TS_SQLConnAnchor(new TS_SQLConnConfig("test"));
+        TGS_RunnableType2<String, Integer> progressUpdate_with_userDotTable_and_percentage = (userDotTable, percentage) -> {
+            var clearPercentages = percentage == TS_FileTmcrParser.CLEAR_PERCENTAGES();
+            d.cr("main", "progressUpdate_with_userDotTable_and_percentage", "userDotTable", userDotTable, "percentage", clearPercentages ? "clearPercentages" : percentage);
+        };
+        TGS_RunnableType1<TS_FileTmcrFileHandler> exeBeforeZip = fileHandler -> {
+            d.cr("main", "exeBeforeZip", "nothing to do");
+        };
+        TGS_RunnableType1<TS_FileTmcrFileHandler> exeAfterZip = fileHandler -> {
+            d.cr("main", "exeAfterZip", "nothing to do");
+        };
         var result = TS_FileTmcrFileHandler.use(
-                fileCommonBall,
-                null,//TS_SQLConnAnchor anchor, 
-                null,//<String, Integer> progressUpdate_with_userDotTable_and_percentage, 
-                null,//<TS_FileTmcrFileHandler> exeBeforeZip, 
-                null//<TS_FileTmcrFileHandler> exeAfterZip
+                fileCommonBall, anchor,
+                progressUpdate_with_userDotTable_and_percentage,
+                exeBeforeZip, exeAfterZip
         );
         d.cr("main", "result", result);
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.tugalsan.blg.file.tmcr {
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.list;
+    requires com.tugalsan.api.sql.conn;
     requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.url;
     requires com.tugalsan.api.file.common;
     requires com.tugalsan.lib.file.tmcr;
