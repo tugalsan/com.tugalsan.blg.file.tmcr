4acfd588b937d440e60dc29fc293da95bfa32270
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-10-21 08:11:04 +0300

    Update Main.java

--- src/main/java/com/tugalsan/blg/file/tmcr/Main.java
+++ src/main/java/com/tugalsan/blg/file/tmcr/Main.java
@@ -95,7 +95,9 @@ public class Main {
         macroCode.addAll(macroCode_addTable(dirDatUsrImageLandscape));
         macroCode.addAll(macroCode_addTable(dirDatUsrImagePortrait));
         var config = toConfig(favIconText, macroCode);
+        var useOpenPdf = true;
         var result = TS_LibFileTmcrFileHandler.use(
+                useOpenPdf,
                 config,
                 createDbAnchor("test"),
                 progressUpdate,
